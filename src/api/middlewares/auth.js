23368482d74b80161e6c7d6228a977813648069a
BlenchuRot <belen.aguirre.de.carcer.rojo@gmail.com>
2020-06-11 12:26:08 +0200

    fix auth middleware

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -1,24 +1,24 @@
-  // TODO: comprobar si el token de auth es valido
-  // si no es valido 401
-  
-  module.exports = async (req, res, next) => {
+// TODO: comprobar si el token de auth es valido
+// si no es valido 401
+
+module.exports = async (req, res, next) => {
   const { jwt, config } = req.$;
   const auth = req.get('Authorization');
   if (!auth || !auth.startsWith('Bearer ')) {
-      res.status(401).end();
-      return;
+    res.status(401).end();
+    return;
   }
   // si es valido grabar el usuario en req.$ y next
   const { authSecret,
-   } = config.authentication;
+  } = config.authentication;
+  try {
+    const token = auth.split(' ')[1];
+    const user = await jwt.verify(token, authSecret);
+    req.$.user = user;
+    next();
 
-  const token = auth.split(' ')[1];
-  const user = await jwt.verify(token, authSecret);
- 
-  if(!user){
-      res.status(401).end();
-      return;
+  } catch (err) {
+    res.status(401).end();
+    console.error(err)    
   }
-  req.$.user = user;
-  next();
 }
